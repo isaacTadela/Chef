f7ba7cf344c52b66db941be7b5e4e74baf4e0c3d
isaacTadela <47701407+isaacTadela@users.noreply.github.com>
2021-12-07 23:54:23 +0200

    add Configure file as system service

--- script/consul-installation.sh
+++ script/consul-installation.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
- wget https://releases.hashicorp.com/consul/1.10.4/consul_1.10.4_linux_amd64.zip
- unzip consul_1.10.4_linux_amd64.zip
- rm consul_1.10.4_linux_amd64.zip
- sudo mv consul /usr/local/bin/
+
+wget https://releases.hashicorp.com/consul/1.10.4/consul_1.10.4_linux_amd64.zip
+unzip consul_1.10.4_linux_amd64.zip
+rm consul_1.10.4_linux_amd64.zip
+sudo mv consul /usr/local/bin/
+
+# Configure client agent as system service
+cat > /etc/systemd/system/consul.service <<EOF
+[Unit]
+Description=HashiCorp Consul Client - A service mesh solution
+Requires=network-online.target
+After=network-online.target
+Documentation=https://www.consul.io/
+[Service]
+EnvironmentFile=-/etc/sysconfig/consul
+Restart=on-failure
+ExecStart=/usr/local/bin/consul agent -retry-join '13.38.98.93' -data-dir=/var/lib/consul -config-dir=/etc/consul.d -bind=0.0.0.0 -datacenter=my_dc -client=0.0.0.0
+ExecReload=/bin/kill -HUP $MAINPID
+LimitNOFILE=65536
+[Install]
+WantedBy=multi-user.target
+EOF
