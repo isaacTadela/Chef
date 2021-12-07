ddfe6cca78e861e716468406e03c2a022f432037
isaacTadela <47701407+isaacTadela@users.noreply.github.com>
2021-12-08 01:19:05 +0200

    Update consul-installation.sh

--- script/consul-installation.sh
+++ script/consul-installation.sh
@@ -6,18 +6,20 @@ rm consul_1.10.4_linux_amd64.zip
 sudo mv consul /usr/local/bin/
 
 # Configure client agent as system service
-cat > /etc/systemd/system/consul.service <<EOF
+echo "
 [Unit]
 Description=HashiCorp Consul Client - A service mesh solution
 Requires=network-online.target
 After=network-online.target
 Documentation=https://www.consul.io/
+
 [Service]
 EnvironmentFile=-/etc/sysconfig/consul
 Restart=on-failure
-ExecStart=/usr/local/bin/consul agent -retry-join "$MASTER_IP" -data-dir=/var/lib/consul -config-dir=/etc/consul.d -bind=0.0.0.0 -datacenter=my_dc -client=0.0.0.0
+ExecStart=/usr/local/bin/consul agent -retry-join $MASTER_IP -data-dir=/var/lib/consul -config-dir=/etc/consul.d -bind=0.0.0.0 -datacenter=my_dc -client=0.0.0.0
 ExecReload=/bin/kill -HUP $MAINPID
 LimitNOFILE=65536
+
 [Install]
 WantedBy=multi-user.target
-EOF
+" > /etc/systemd/system/consul.service
