35a72f3b7e687e70a03b1694931b42cd25e912b4
Ubuntu <ubuntu@ip-172-31-31-181.eu-west-3.compute.internal>
2021-12-08 19:45:32 +0000

    fix the install-consul script

--- script/consul-installation.sh
+++ script/consul-installation.sh
@@ -6,24 +6,19 @@ rm consul_1.10.4_linux_amd64.zip
 sudo mv consul /usr/local/bin/
 
 # Configure client agent as system service
-echo "
-[Unit]
+echo "[Unit]
 Description=HashiCorp Consul Client - A service mesh solution
 Requires=network-online.target
 After=network-online.target
 Documentation=https://www.consul.io/
-
 [Service]
 EnvironmentFile=-/etc/sysconfig/consul
 Restart=on-failure
-ExecStart=/usr/local/bin/consul agent -retry-join $MASTER_IP -data-dir=/var/lib/consul -config-dir=/etc/consul.d -bind=$MASTER_IP -datacenter=my_dc
+ExecStart=/usr/local/bin/consul agent -retry-join '$MASTER_IP' -data-dir=/var/lib/consul -config-dir=/etc/consul.d -bind=0.0.0.0 -datacenter=my_dc -client=0.0.0.0
 ExecReload=/bin/kill -HUP $MAINPID
 LimitNOFILE=65536
-
 [Install]
-WantedBy=multi-user.target
-
-" > /etc/systemd/system/consul.service
+WantedBy=multi-user.target" > /etc/systemd/system/consul.service
 
 
 sudo mkdir /etc/consul.d
